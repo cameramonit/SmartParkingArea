dc87fa7786b52628b2b14203d869c1c2486a8109
RadenMas6699 <radenmas.dev@gmail.com>
2022-04-14 01:21:24 +0800

    Initial commit

---
+++ app/src/main/java/com/radenmas/smart_parking_area/ui/admin/AdminMainActivity.kt
@@ -0,0 +1,12 @@
+package com.radenmas.smart_parking_area.ui.admin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.radenmas.smart_parking_area.R
+
+class AdminMainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_admin_main)
+    }
+}
